1425569b9a2809e76dfc15ee913450c431c689ab
WagnerWD40 <wagnerwd40@gmail.com>
2020-07-10 21:17:50 -0300

    added dashboard layout

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './styles.css';
+import MaterialIcon from 'material-icons-react';
 
 import Background from '../../components/Background';
 import useInputState from '../../hooks/useInputState';
@@ -17,6 +18,7 @@ import {
 export default function Dashboard({ user, setUser }) {
     const [chatroomName, changeHandleChatroomName, resetChatroomName] = useInputState('');
     const [chatrooms, setChatrooms] = useState([]);
+    const [createFormVisibility, setCreateFormVisibility] = useState(true);
 
     const history = useHistory();
 
@@ -38,25 +40,77 @@ export default function Dashboard({ user, setUser }) {
         history.push(`/chat/${chatroomName}`);
     };
 
+    function handleClickAddChatroom() {
+        setCreateFormVisibility(!createFormVisibility);
+    };
+
     return (
         <Background>
-            usuário
-            <form onSubmit={handleSubmit}>
-                <input value={chatroomName} onChange={changeHandleChatroomName} />
-                <button type="submit">create new Chatroom</button>
+            <nav className="Dashboard-navbar">
+                <ul className="Dashboard-iconrow">
+                    <li>
+                        <MaterialIcon
+                                icon="more_vert"
+                                color="#25c5fa"/>   
+                    </li>
+                    <li>
+                        <MaterialIcon
+                                icon="person"
+                                color="#25c5fa"/>   
+                    </li>
+                    <li onClick={handleClickAddChatroom}>
+                        <MaterialIcon
+                                icon="queue"
+                                color="#25c5fa"/>   
+                    </li>
+                    <li>
+                        <MaterialIcon
+                                icon="settings"
+                                color="#25c5fa"/>   
+                    </li>
+                    <li>
+                        <MaterialIcon
+                                icon="meeting_room"
+                                color="#25c5fa"/>   
+                    </li>
+                </ul>              
+            </nav>
+            <form 
+                onSubmit={handleSubmit}
+                className={createFormVisibility
+                            ? "Dashboard-create-chatroom-form"
+                            : "Dashboard-create-chatroom-form Dashboard-hidden"}>
+                <input
+                    value={chatroomName}
+                    onChange={changeHandleChatroomName}
+                    placeholder="New chatroom name..."
+                    className="Dashboard-create-chatroom-input" />
+                <button
+                    type="submit"
+                    className="Dashboard-create-chatroom-button">
+                        <MaterialIcon
+                            icon="add"
+                            color="#25c5fa"/>
+                </button>
             </form>
-            <ul className="ul">
-                {
-                    chatrooms && chatrooms.map(chatroom =>
-                        <li
-                            key={chatroom.name} 
-                            className="Dashboard-chatroom"
-                            onClick={() => handleClickChatroom(chatroom.name)}>
-                            {chatroom.name}
-                        </li>
-                    )
-                }
-            </ul>
+            <div className="Dashboard-chatroom-list-container">
+                <ul className="ul">
+                    {
+                        chatrooms && chatrooms.map(chatroom =>
+                            <li
+                                key={chatroom.name} 
+                                className="Dashboard-chatroom"
+                                onClick={() => handleClickChatroom(chatroom.name)}>
+                                <div className="Dashboard-chatroom-title">
+                                    {chatroom.name}
+                                </div>
+                                <div className="Dashboard-chatroom-content">
+                                </div>
+                            </li>
+                        )
+                    }
+                </ul>
+            </div>
         </Background>
     );
 };
